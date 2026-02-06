ce136de6c7e9e341e009cebb991c286c1655d7e3
Sai deekshith <orugantideekshith2006@gmail.com>
2026-02-06 23:41:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function add() {
   document.getElementById("result").innerText =
     "Result: " + (n1 + n2);
 }
+function subtract() {
+  const n1 = Number(document.getElementById("num1").value);
+  const n2 = Number(document.getElementById("num2").value);
+
+  document.getElementById("result").innerText =
+    "Result: " + (n1 - n2);
+}
