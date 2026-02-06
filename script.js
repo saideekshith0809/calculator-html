dd7092c37460362d03c1484094237213b5f4e18d
Sai deekshith <orugantideekshith2006@gmail.com>
2026-02-06 23:40:57 +0530

    Added addition functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+function add() {
+  const n1 = Number(document.getElementById("num1").value);
+  const n2 = Number(document.getElementById("num2").value);
+
+  document.getElementById("result").innerText =
+    "Result: " + (n1 + n2);
+}
