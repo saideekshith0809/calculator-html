99beca94947a8f586f2d90f50d2646bffa14c5de
Sai deekshith <orugantideekshith2006@gmail.com>
2026-02-13 23:33:30 +0530

    Added multiplication functionality

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ function add() {
 function subtract() {
   const n1 = Number(document.getElementById("num1").value);
   const n2 = Number(document.getElementById("num2").value);
+function multiply() {
+  const n1 = Number(document.getElementById("num1").value);
+  const n2 = Number(document.getElementById("num2").value);
 
+  document.getElementById("result").innerText =
+    "Result: " + (n1 * n2);
+}
+
+
+  
   document.getElementById("result").innerText =
     "Result: " + (n1 - n2);
 }
